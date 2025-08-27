2941a2936cf4d120af702cb50992b27dd851c120
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-08-27 13:55:17 +0700

    create regular text component

--- core/ui/src/main/java/com/example/ui/components/TextUiJello.kt
+++ core/ui/src/main/java/com/example/ui/components/TextUiJello.kt
@@ -1,14 +1,18 @@
 package com.example.ui.components
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -36,10 +40,43 @@ fun JelloTextHeader(
     )
 }
 
-@Preview(device = Devices.NEXUS_5, showSystemUi = true)
+@Preview
 @Composable
 fun JelloTextHeaderPreview() {
     JelloTextHeader(
         text = "Acumalaka xixixxixixii ganteng buanget cik"
     )
+}
+
+@Composable
+fun JelloTextRegularWithClick() {
+    val annotatedText = buildAnnotatedString {
+        append("Please fill E-mail & password to login your app account.")
+
+        pushStringAnnotation(
+            tag = "TEXT_CLICK",
+            annotation = " Sign Up"
+        )
+        withStyle(
+            style = SpanStyle(
+                color = Color.Blue,
+                fontWeight = FontWeight.Bold
+            )
+        ) {
+            append(" Sign Up")
+        }
+        pop()
+    }
+
+    ClickableText(
+        text = annotatedText
+    ) {
+
+    }
+}
+
+@Preview
+@Composable
+fun JelloTextRegularWithClickPreview() {
+    JelloTextRegularWithClick()
 }
