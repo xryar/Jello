02eca927287ba6521cf231e133ab14c9ffac31b2
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-08-30 15:22:31 +0700

    update edit text, set new color

--- core/ui/src/main/java/com/example/ui/components/EditTextUiJello.kt
+++ core/ui/src/main/java/com/example/ui/components/EditTextUiJello.kt
@@ -6,34 +6,49 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ui.theme.PurpleGrey80
+import com.example.ui.theme.VeryLightGray
 
 @Composable
 fun JelloEditText(
-    value: String,
+    value: String = "Email",
     onTyping: (String) -> Unit = {},
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
 ) {
     BasicTextField(
         value = value,
         onValueChange = { onTyping.invoke(it) },
         modifier = Modifier.fillMaxWidth()
             .padding(16.dp)
-            .border(width = 1.dp, color = PurpleGrey80, shape = RoundedCornerShape(8.dp))
+            .border(width = 1.dp, color = VeryLightGray, shape = RoundedCornerShape(8.dp))
             .background(Color.White)
             .padding(8.dp),
-        textStyle = TextStyle(fontSize = 14.sp, color = Color.Black)
+        textStyle = TextStyle(fontSize = 14.sp, color = Color.Black),
+        visualTransformation = visualTransformation,
+        keyboardOptions = keyboardOptions,
     )
 }
 
 @Preview
 @Composable
 fun JelloEditTextPreview() {
-    JelloEditText("Email")
+    JelloEditText()
+}
+
+@Preview
+@Composable
+fun JelloEditTextPasswordPreview() {
+    JelloEditText(visualTransformation = PasswordVisualTransformation())
 }
