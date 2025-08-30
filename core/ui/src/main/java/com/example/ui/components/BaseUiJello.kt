bac0a7f15e49aca8ae6a8b62ebacfe8a99b7b2be
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-08-30 18:04:16 +0700

    update button with icon component

--- core/ui/src/main/java/com/example/ui/components/BaseUiJello.kt
+++ core/ui/src/main/java/com/example/ui/components/BaseUiJello.kt
@@ -1,16 +1,20 @@
 package com.example.ui.components
 
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.ui.R
 
 @Composable
 fun JelloBaseButton(
@@ -42,11 +46,32 @@ fun JelloBaseButtonPreview() {
 }
 
 @Composable
-fun JelloWithIconBaseButton() {
+fun JelloWithIconBaseButton(
+    modifier: Modifier = Modifier,
+    text:String = "Default Text",
+    onClick: () -> Unit = {},
+    enabled: Boolean = true,
+    shape: Shape = RoundedCornerShape(8.dp),
+    colors: ButtonColors = ButtonDefaults.buttonColors(
+        containerColor = Color.Blue,
+        contentColor = Color.White,
+    ),
+    color: Color = Color.White,
+) {
     Button(
-        onClick = {}
+        modifier = modifier,
+        enabled = enabled,
+        shape = shape,
+        colors = colors,
+        onClick = { onClick }
     ) {
-        Text("Login Now")
+        Icon(
+            painter = painterResource(id = R.drawable.ic_facebook),
+            contentDescription = "Facebook",
+            modifier = Modifier.size(24.dp),
+            tint = color
+        )
+        Text(text)
     }
 }
 
