19a011726e4406a155308b2bec1e6e5b100c7ba7
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-08-31 17:46:49 +0700

    remove unused import

--- core/ui/src/main/java/com/example/ui/components/ButtonUiJello.kt
+++ core/ui/src/main/java/com/example/ui/components/ButtonUiJello.kt
@@ -3,11 +3,9 @@ package com.example.ui.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.ui.theme.LightOrange
