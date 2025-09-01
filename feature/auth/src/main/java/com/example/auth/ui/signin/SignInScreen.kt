f3e39ae45f4207ca21f732ca4fb4d48764ab0297
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-01 11:43:07 +0700

    remove unused import

--- feature/auth/src/main/java/com/example/auth/ui/signin/SignInScreen.kt
+++ feature/auth/src/main/java/com/example/auth/ui/signin/SignInScreen.kt
@@ -10,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
