5225d054b51dc09611dc2d796c3cb2fd4a0e11b1
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-01 20:02:36 +0700

    remove unused import

--- feature/home/src/main/java/com/example/home/HomeActivity.kt
+++ feature/home/src/main/java/com/example/home/HomeActivity.kt
@@ -3,7 +3,6 @@ package com.example.home
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Surface
 import com.example.home.ui.MainScreen
 import com.example.ui.theme.JelloTheme
