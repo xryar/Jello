4027ab2e2a0f2e111ef92a622c77720fd0668226
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-02 09:13:39 +0700

    search bar component implemented

--- feature/home/src/main/java/com/example/home/ui/home/HomeScreen.kt
+++ feature/home/src/main/java/com/example/home/ui/home/HomeScreen.kt
@@ -1,25 +1,75 @@
 package com.example.home.ui.home
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.ui.components.JelloImageViewClick
 import com.example.ui.components.JelloTextRegular
+import com.example.ui.theme.StrongBlue
 
 @Composable
 fun HomeScreen() {
-    Column {
-        Row {
-            Card {
-                Row {
-                    JelloImageViewClick()
-                    JelloTextRegular()
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(StrongBlue)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .clickable{
+
+                    },
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.White.copy(alpha = 0.2f)
+                )
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    JelloImageViewClick(
+                        color = Color.White,
+                        imageVector = Icons.Outlined.Search,
+                        onClick = {
+
+                        }
+                    )
+
+                    JelloTextRegular(
+                        text = "Cari barang kamu disini",
+                        color = Color.White,
+                    )
                 }
             }
-            JelloImageViewClick()
+            JelloImageViewClick(
+                color = Color.White,
+                imageVector = Icons.Outlined.ShoppingCart,
+                onClick = {
+
+                }
+            )
         }
     }
 }
