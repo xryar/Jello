9eb0794dfd768e3fbbef456840ecd3feeb11171c
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-03 09:14:09 +0700

    create product search bar component

--- feature/home/src/main/java/com/example/home/ui/product/ProductScreen.kt
+++ feature/home/src/main/java/com/example/home/ui/product/ProductScreen.kt
@@ -1,16 +1,78 @@
 package com.example.home.ui.product
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.home.ui.ScreenContent
+import com.example.ui.components.JelloImageViewClick
+import com.example.ui.components.JelloTextRegular
+import com.example.ui.theme.Gray
 
 
 @Composable
 fun ProductScreen() {
-    ScreenContent("Product")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .clickable{
+
+                    },
+                colors = CardDefaults.cardColors(
+                    containerColor = Gray.copy(alpha = 0.1f)
+                )
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .fillMaxWidth()
+                ) {
+                    JelloTextRegular(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.CenterVertically)
+                            .weight(1f),
+                        text = "Cari barang kamu disini",
+                        color = Gray
+                    )
+                    JelloImageViewClick(
+                        color = Gray,
+                        onClick = {
+                            
+                        },
+                        imageVector = Icons.Outlined.Search
+                    )
+                }
+            }
+        }
+    }
 }
 
-@Preview
+@Preview(showSystemUi = true)
 @Composable
 fun ProductScreenPreview() {
     ProductScreen()
