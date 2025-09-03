5f7a1a395343283c63619defd7d63cba38d3d970
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-03 09:35:38 +0700

    remove unused import

--- feature/home/src/main/java/com/example/home/ui/product/ProductScreen.kt
+++ feature/home/src/main/java/com/example/home/ui/product/ProductScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Divider
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
