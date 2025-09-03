2e13b07e4249a2e04e4bc5e77358d674d8cd6dfd
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-03 13:28:23 +0700

    add header and search bar in order screen

--- feature/home/src/main/java/com/example/home/ui/order/OrderScreen.kt
+++ feature/home/src/main/java/com/example/home/ui/order/OrderScreen.kt
@@ -1,12 +1,109 @@
 package com.example.home.ui.order
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.home.R
 import com.example.home.ui.ScreenContent
+import com.example.home.ui.product.ItemProduct
+import com.example.ui.components.JelloImageViewClick
+import com.example.ui.components.JelloTextRegular
+import com.example.ui.theme.Gray
+import com.example.ui.theme.LightGrayishBlue
+import com.example.ui.theme.VeryLightGray
 
 @Composable
 fun OrderScreen() {
-    ScreenContent("Order")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .clickable{
+
+                    },
+                colors = CardDefaults.cardColors(
+                    containerColor = Gray.copy(alpha = 0.1f)
+                )
+            ) {
+                Row(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .fillMaxWidth()
+                ) {
+                    JelloTextRegular(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .align(Alignment.CenterVertically)
+                            .weight(1f),
+                        text = "Cari barang kamu disini",
+                        color = Gray
+                    )
+                    JelloImageViewClick(
+                        color = Gray,
+                        onClick = {
+
+                        },
+                        imageVector = Icons.Outlined.Search
+                    )
+                }
+            }
+        }
+
+        HorizontalDivider(
+            modifier = Modifier
+                .fillMaxWidth(),
+            thickness = 2.dp,
+            color = VeryLightGray
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 25.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            JelloTextRegular(
+                text = "NEW PRODUCT",
+                modifier = Modifier.weight(1f)
+            )
+
+            JelloImageViewClick(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_filter),
+                color = LightGrayishBlue
+            )
+            JelloImageViewClick(
+                imageVector = ImageVector.vectorResource(R.drawable.ic_catalog),
+                color = LightGrayishBlue
+            )
+        }
+    }
 }
 
 @Preview
