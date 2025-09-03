5fc107d453abd44e6d4bd72631ca3c7cb1e5d994
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-03 09:35:23 +0700

    add divider

--- feature/home/src/main/java/com/example/home/ui/product/ProductScreen.kt
+++ feature/home/src/main/java/com/example/home/ui/product/ProductScreen.kt
@@ -11,6 +11,8 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -20,6 +22,7 @@ import androidx.compose.ui.unit.dp
 import com.example.ui.components.JelloImageViewClick
 import com.example.ui.components.JelloTextRegular
 import com.example.ui.theme.Gray
+import com.example.ui.theme.VeryLightGray
 
 
 @Composable
@@ -68,6 +71,13 @@ fun ProductScreen() {
                 }
             }
         }
+
+        HorizontalDivider(
+            modifier = Modifier
+                .fillMaxWidth(),
+            thickness = 2.dp,
+            color = VeryLightGray
+        )
     }
 }
 
