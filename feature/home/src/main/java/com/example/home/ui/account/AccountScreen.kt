c680d0c04a0e214fdf9d3eb362d3d25d1eb19e79
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-05 17:21:02 +0700

    remove unused import

--- feature/home/src/main/java/com/example/home/ui/account/AccountScreen.kt
+++ feature/home/src/main/java/com/example/home/ui/account/AccountScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.home.ui.ScreenContent
 import com.example.ui.components.JelloImageViewClick
 import com.example.ui.components.JelloTextRegular
 import com.example.ui.theme.StrongBlue
