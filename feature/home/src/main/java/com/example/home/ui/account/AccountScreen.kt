104056f0d35848dc7f8787d220f853c347fb6c79
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-05 17:24:45 +0700

    add new icon in account screen

--- feature/home/src/main/java/com/example/home/ui/account/AccountScreen.kt
+++ feature/home/src/main/java/com/example/home/ui/account/AccountScreen.kt
@@ -10,8 +10,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.home.R
 import com.example.ui.components.JelloImageViewClick
 import com.example.ui.components.JelloTextRegular
 import com.example.ui.theme.StrongBlue
@@ -46,7 +49,8 @@ fun AccountScreen() {
                 color = Color.White,
                 onClick = {
 
-                }
+                },
+                imageVector = ImageVector.vectorResource(R.drawable.ic_logout)
             )
         }
     }
