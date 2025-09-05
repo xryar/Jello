dd1c1542f67c95d533a7db4d0035ff1e81bd6324
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-05 18:32:46 +0700

    remove unused import

--- feature/home/src/main/java/com/example/home/ui/account/AccountScreen.kt
+++ feature/home/src/main/java/com/example/home/ui/account/AccountScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
-import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
