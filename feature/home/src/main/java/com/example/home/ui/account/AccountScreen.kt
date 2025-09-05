6942832519189577d18110fd9fc837af499f8bfc
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-05 17:20:47 +0700

    create header content in account screen

--- feature/home/src/main/java/com/example/home/ui/account/AccountScreen.kt
+++ feature/home/src/main/java/com/example/home/ui/account/AccountScreen.kt
@@ -1,12 +1,56 @@
 package com.example.home.ui.account
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.home.ui.ScreenContent
+import com.example.ui.components.JelloImageViewClick
+import com.example.ui.components.JelloTextRegular
+import com.example.ui.theme.StrongBlue
 
 @Composable
 fun AccountScreen() {
-    ScreenContent("Account")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(StrongBlue),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            JelloImageViewClick(
+                color = Color.White,
+                onClick = {
+
+                }
+            )
+            JelloTextRegular(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.CenterVertically)
+                    .weight(1f),
+                text = "Profile",
+                color = Color.White
+            )
+            JelloImageViewClick(
+                color = Color.White,
+                onClick = {
+
+                }
+            )
+        }
+    }
 }
 
 @Preview
