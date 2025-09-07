f4a60a4c4d008337cc3c84b624d7bb0ac6d6f8b6
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-07 19:05:26 +0700

    create network response adapter

--- app/src/main/java/com/example/jello/utils/retrofit/NetworkResponseAdapter.kt
+++ app/src/main/java/com/example/jello/utils/retrofit/NetworkResponseAdapter.kt
@@ -1,4 +1,20 @@
 package com.example.jello.utils.retrofit
 
-class NetworkResponseAdapter {
+import com.example.remote.model.base.NetworkResource
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.CallAdapter
+import retrofit2.Converter
+import java.lang.reflect.Type
+
+class NetworkResponseAdapter<S: Any, E: Any>(
+    private val successType: Type,
+    private val errorBodyConverter: Converter<ResponseBody, E>
+): CallAdapter<S, Call<NetworkResource<S, E>>> {
+    override fun responseType(): Type = successType
+
+    override fun adapt(call: Call<S>): Call<NetworkResource<S, E>> {
+        return NetworkResourceCall(call, errorBodyConverter)
+    }
+
 }
