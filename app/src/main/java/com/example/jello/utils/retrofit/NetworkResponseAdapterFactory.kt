5d79546b655ef5d3a72c2714d3781931e872a23c
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-07 19:18:27 +0700

    create network response adapter factory

--- app/src/main/java/com/example/jello/utils/retrofit/NetworkResponseAdapterFactory.kt
+++ app/src/main/java/com/example/jello/utils/retrofit/NetworkResponseAdapterFactory.kt
@@ -1,4 +1,43 @@
 package com.example.jello.utils.retrofit
 
-class NetworkResponseAdapterFactory {
+import com.example.remote.model.base.NetworkResource
+import retrofit2.Call
+import retrofit2.CallAdapter
+import retrofit2.Retrofit
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+
+class NetworkResponseAdapterFactory: CallAdapter.Factory() {
+
+    override fun get(
+        returnType: Type,
+        annotations: Array<out Annotation?>,
+        retrofit: Retrofit
+    ): CallAdapter<*, *>? {
+
+        if (Call::class.java != getRawType(returnType)) {
+            return null
+        }
+
+        check(returnType is ParameterizedType) {
+            "return type must be parameterized as Call<NetworkResource<<V>> or Call<NetworkResource<out V>>"
+        }
+
+        val responseType = getParameterUpperBound(0, returnType)
+
+        if (getRawType(responseType) != NetworkResource::class.java) {
+            return null
+        }
+
+        check(responseType is ParameterizedType) {
+            "NetworkResource must be parameterized as NetworkResource<V> or NetworkResource<out V>"
+        }
+
+        val successBodyType = getParameterUpperBound(0, responseType)
+        val errorBodyType = getParameterUpperBound(1, responseType)
+        val errorBodyConverter = retrofit.nextResponseBodyConverter<Any>(null, errorBodyType, annotations)
+
+        return NetworkResponseAdapter<Any, Any>(successBodyType, errorBodyConverter)
+    }
+
 }
