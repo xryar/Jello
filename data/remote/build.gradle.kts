a29e60a920250acbc5493794f17b52d95db4ec6b
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-07 13:23:54 +0700

    fix typo in gradle

--- data/remote/build.gradle.kts
+++ data/remote/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
+    implementation(libs.gson)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
--- data/remote/src/main/java/com/example/remote/model/base/ErrorResponse.kt
+++ data/remote/src/main/java/com/example/remote/model/base/ErrorResponse.kt
@@ -1,5 +1,7 @@
 package com.example.remote.model.base
 
+import com.google.gson.annotations.SerializedName
+
 data class ErrorResponse(
     @SerializedName("code")
     val code:  Int ? = null,
