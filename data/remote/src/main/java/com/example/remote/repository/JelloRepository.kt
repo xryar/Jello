7e169e275163074058fe58c48df49b15cc8798d5
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-08 11:54:49 +0700

    import `JelloLoginDomainModel` in `JelloRepository`

--- data/remote/src/main/java/com/example/remote/repository/JelloRepository.kt
+++ data/remote/src/main/java/com/example/remote/repository/JelloRepository.kt
@@ -1,6 +1,7 @@
 package com.example.remote.repository
 
 import com.example.domain.model.base.DomainResource
+import com.example.domain.model.domain.JelloLoginDomainModel
 
 interface JelloRepository {
 
