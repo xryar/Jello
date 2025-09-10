9862ea3bcf14e3df8014583f04f4be3b23d57d4a
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-10 19:29:52 +0700

    Create preference module provide preference context

--- data/local/src/main/java/com/example/local/di/PreferenceModule.kt
+++ data/local/src/main/java/com/example/local/di/PreferenceModule.kt
@@ -1,4 +1,24 @@
 package com.example.local.di
 
+import android.content.Context
+import com.example.local.preference.SharedPreference
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 class PreferenceModule {
+
+    @Provides
+    @Singleton
+    fun providePreference(
+        @ApplicationContext context: Context
+    ): SharedPreference {
+        return SharedPreference(context)
+    }
+
 }
