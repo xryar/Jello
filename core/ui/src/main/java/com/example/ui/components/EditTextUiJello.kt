17d59d0bd0ea4991edb4a9a8bbad10033eeb1ce4
Arya Rizki Andaru <aryarizki3045@gmail.com>
2025-09-10 16:38:59 +0700

    remove unused import

--- core/ui/src/main/java/com/example/ui/components/EditTextUiJello.kt
+++ core/ui/src/main/java/com/example/ui/components/EditTextUiJello.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
